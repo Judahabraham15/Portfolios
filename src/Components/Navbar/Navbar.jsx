22c5029543b0e292d4ea1055ff3495dbb4761ea0
Judah Abraham <judahabrahamforgood@gmail.com>
2025-06-05 17:49:41 +0100

    Started creatiing the porfolio

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <h1 className='nav-header'>Judah4Good</h1>
+
+        <ul className="lists">
+            <a href='#'></a>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
