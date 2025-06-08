f75851a4ad0c6b86ceb387cb24b5f473f93efc81
Judah Abraham <judahabrahamforgood@gmail.com>
2025-06-08 13:11:09 +0100

    Added chnages

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import Spline from '@splinetool/react-spline';
 import './Hero.css'
+import { useEffect } from 'react';
 const Hero = () => {
+  useEffect(()=>{
+    const textElement = document.getElementById('ChangeText');
+  const textArray = ['Smart Development' ,  'Innovative Solutions' ,'Digital Systems']
+    let textIndex = 0;
+
+    const ChangeText = () => {
+      textElement.style.opacity= 0;
+      setTimeout(()=>{
+       textElement.textContent = textArray[textIndex];
+       textIndex = (textIndex + 1  ) % textArray.length
+       textElement.style.opacity = 1;
+       textElement.style.color = '#2C5364';
+      } , 500)
+    }
+    const interval = setInterval(()=>{
+ChangeText()
+    }  , 2000)
+    return () => clearInterval (interval);
+    
+  }, [])
   return (
     <div className='hero'>
      <div className="content">
-        <h1>Hi , I am Judah Abraham</h1>
-        <p>Dictate your products with Judah4Good</p>
+        <h1>Unlock Potential through{''} <span id='ChangeText'>Technology</span> with Judah4Good</h1>
+        <p>As a dedicated and aspiring software developer, I combine technical expertise with creative vision to build software that is both powerful and intuitive. With a focus on quality  , reliability , and user experience, I deliver solutions that meet the highest standards of excellence and drive buisness success.</p>
      </div>
      <div className="spline">
-        <Spline
-        scene="https://prod.spline.design/oY1prPE9wputgk4R/scene.splinecode" 
+         <Spline
+        scene="https://prod.spline.design/GlU9mQH8ujn-nfgQ/scene.splinecode" 
       />
       </div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,16 +3,15 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+
       <div className="nav">
           <h1 className='nav-header'>Judah4Good</h1>
-
         <ul className="lists">
             <a href='#'></a>
         </ul>
       </div>
       
-    </div>
+   
   )
 }
 
