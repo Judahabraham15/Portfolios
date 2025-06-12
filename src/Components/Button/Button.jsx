062a6df308ef348ca8c12cae8e63d945e19045d8
Judah Abraham <judahabrahamforgood@gmail.com>
2025-06-12 15:49:10 +0100

    More features

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import './Button.css'
 
 
-const Button = ({title , style}) => {
+const Button = ({title , style , className}) => {
   return (
     <div className='btn'>
- <button className='button2' href='#'>Explore More</button>
+ <button className={`button2 ${className}`} style={{style}}>{title}
+  <div className="icon">
+    <svg
+      height="24"
+      width="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 0h24v24H0z" fill="none"></path>
+      <path
+        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  </div>
+ </button>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,12 +4,8 @@ import './Navbar.css'
 const Navbar = () => {
   return (
 
-      <div className="nav">
-          <h1 className='nav-header'>Judah4Good</h1>
-        <ul className="lists">
-            <a href='#'></a>
-        </ul>
-      </div>
+      <>
+    </>
       
    
   )
