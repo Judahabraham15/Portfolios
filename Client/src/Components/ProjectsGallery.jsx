df1f7d0b03245b3020f4450365b65c08b9e310a4
Judah Abraham <judahabrahamforgood@gmail.com>
2025-11-20 11:28:37 +0100

    Update ProjectsGallery component: adjust height and replace placeholder images with project URLs

--- Client/src/Components/ProjectsGallery.jsx
+++ Client/src/Components/ProjectsGallery.jsx
@@ -4,7 +4,7 @@ import { InfiniteMovingCards } from "./UI/MovingCards";
 import Sam from  '../assets/Sam.png'
 const ProjectsGallery = () => {
   return (
-     <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+     <div className="h-[20rem] mb-[10rem] flex flex-col antialiased items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items={projects}
         direction="right"
@@ -18,19 +18,19 @@ export default ProjectsGallery;
 
 const projects = [
   {
-    src: Sam,
+    src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763627962/port_ckjluh.png",
     alt: "Project 1",
   },
   {
-    src: Sam,
+    src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763627842/Blink_zspbte.png",
     alt: "Project 2",
   },
   {
-    src: Sam,
+    src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763633342/Screenshot_2025-11-20_105948_vnxvlg.png",
     alt: "Project 3",
   },
   {
-    src: Sam,
+    src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763632234/Screenshot_2025-11-20_104944_uxotyh.png",
     alt: "Project 4",
   },
 
