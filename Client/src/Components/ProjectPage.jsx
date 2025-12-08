9e52f9787462eb5e6f0e4ab5dac380b078401540
Judah Abraham <judahabrahamforgood@gmail.com>
2025-12-08 05:08:46 +0100

    Added a projectPage for routing

---
+++ Client/src/Components/ProjectPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects2 = () => {
+  return (
+    <div className='flex w-full min-h-screen items-center justify-center'>Projects2</div>
+  )
+}
+
+export default Projects2
