83a31463619de89d2cc06b64b9f0d912568e900a
Judah Abraham <judahabrahamforgood@gmail.com>
2025-12-11 09:32:52 +0100

    Deleted both Sections and added a project data section

--- Client/src/Components/ProjectPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Projects2 = () => {
-  return (
-    <div className='flex w-full min-h-screen items-center justify-center'>Projects2</div>
-  )
-}
-
-export default Projects2
---
+++ Client/src/Components/data/projectdata.js
@@ -0,0 +1,35 @@
+const projects = [
+  {
+    slug: "medicx",
+    img: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763632234/Screenshot_2025-11-20_104944_uxotyh.png",
+    title: "MedicX",
+    desc: "A healthcare startup dedicated to improving medical accessibility and quality of life.",
+    icons: ["tailwind", "react", "js", "hubspot"],
+    link: "https://medicx.app",
+  },
+  {
+    slug: "jshare",
+    img: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763633342/Screenshot_2025-11-20_105948_vnxvlg.png",
+    title: "JShare",
+    desc: "A streamlined file-sharing solution designed for fast, effortless collaboration.",
+    icons: ["js", "react", "tailwind", "express"],
+    link: "https://weather-app-oapd.vercel.app/",
+  },
+  {
+    slug: "blinkist",
+    img: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763627842/Blink_zspbte.png",
+    title: "Blinkist-Landing Page",
+    desc: "A Blinkist-Landing Page with more features and a modern Login Page",
+    icons: ["css3", "react", "js"],
+    link: "https://blinkist-landing-page-seven.vercel.app/",
+  },
+  {
+    slug: "ikechukwu-portfolio",
+    img: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763627962/port_ckjluh.png",
+    title: "Ikechukwu Portfolio",
+    desc: "A website that displays the skills of a pharmacist who transitioned to tech",
+    icons: ["react", "css3", "js"],
+    link: "https://ikechwukwu-portfolio.vercel.app/",
+  },
+];
+export default projects
