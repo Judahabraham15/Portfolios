00b7def91d782e546b323ea1a093a0fd2d293319
Judah Abraham <judahabrahamforgood@gmail.com>
2025-12-16 08:16:22 +0100

    modified

--- Client/src/Components/TechCarousel.jsx
+++ Client/src/Components/TechCarousel.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import { SiStripe, SiNodedotjs, SiJavascript, SiFirebase, SiMongodb, SiPython, SiReact, SiNextdotjs } from 'react-icons/si';
+import React from "react";
+import {
+  SiTailwindcss,
+  SiNodedotjs,
+  SiJavascript,
+  SiFirebase,
+  SiMongodb,
+  SiPython,
+  SiReact,
+  SiNextdotjs,
+  SiTypescript,
+} from "react-icons/si";
 
 const TechCarousel = () => {
   const logos = [
-    { name: 'Node.js', Icon: SiNodedotjs },
-    { name: 'JavaScript', Icon: SiJavascript},
-    { name: 'MongoDB', Icon: SiMongodb },
-    { name: 'React', Icon: SiReact },
-    { name: 'Next.js', Icon: SiNextdotjs },
+    { name: "Node.js", Icon: SiNodedotjs },
+    { name: "JavaScript", Icon: SiJavascript },
+    { name: "MongoDB", Icon: SiMongodb },
+    { name: "React", Icon: SiReact },
+    { name: "Next.js", Icon: SiTypescript },
+    { name: "TailwindCss", Icon: SiTailwindcss },
   ];
 
   // Duplicate the logos array multiple times for truly seamless scrolling
   const duplicatedLogos = [...logos, ...logos, ...logos, ...logos];
 
   return (
-    <div className=" flex items-center justify-center overflow-hidden">
-      <div className="relative w-full max-w-md rounded-xs overflow-hidden">
+    <div className="h-32 flex items-center justify-center overflow-hidden">
+      <div className="relative w-full max-w-3xs sm:max-w-md rounded-xs overflow-hidden">
         <div className="flex gap-5 animate-scroll">
           {duplicatedLogos.map((logo, index) => {
             const Icon = logo.Icon;
             return (
-              <div
-                key={index}
-                className="flex-shrink-0"
-              >
+              <div key={index} className="flex-shrink-0">
                 <div className="w-10 h-10 bg-gradient-to-br from-[#2E8B57]  to-neutral-800 rounded-lg p-2 flex items-center justify-center">
                   <Icon className="w-full h-full text-gray-200" />
                 </div>
@@ -32,7 +40,7 @@ const TechCarousel = () => {
           })}
         </div>
       </div>
-      
+
       <style>{`
         @keyframes scroll {
           0% {
