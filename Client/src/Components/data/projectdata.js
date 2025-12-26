28ef12cf8808a3b1db86eb527660b62ce732b884
Judah Abraham <judahabrahamforgood@gmail.com>
2025-12-26 18:21:11 +0100

    updated

--- Client/src/Components/data/projectdata.js
+++ Client/src/Components/data/projectdata.js
@@ -37,6 +37,9 @@ const projects = [
     ],
     para1:
       "Jshare is a modern file upload platform built with React, Typescript, Express and ImageKit. It features drap-and-drop functionality, secure cloud storage with a clean, responsive interface.",
+    para2:
+      "Many people struggle with slow, complicated file sharing tools that create unnecessary delays in their work and communication. Whether it's a cumbersome upload processes, confusing interfaces, or unreliable transfers, these friction points waste time and hinder productivity. There was a clear need for a streamlined solution that makes file sharing genuinely fast and effortless-so I built Jshare using React, Typescript, Express.js, and ImageKit to solve exactly that!",
+    para3: "Jshare eliminates file sharing friction with a fast, intuitive platform built for seamless uploads. Powered by React, Typescript, Express.js, and ImageKit's robust CDN infrastructure, it delivers drag and drop simplicity, and reliable cloud storage. Users can share files instantly without the hassle-no complicated steps, no delays, just effortless uploading that keeps workflows moving.",
     link: "https://j-upload.vercel.app/",
     githubLink: "https://github.com/Judahabraham15/J-Uploads",
   },
