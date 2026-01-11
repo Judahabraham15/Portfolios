00be22890a349dd6fe7f9d5429ee6b1fadca2199
Judah Abraham <judahabrahamforgood@gmail.com>
2026-01-11 13:23:26 +0100

    Updated ProjectGallery

--- Client/src/Components/ProjectsGallery.jsx
+++ Client/src/Components/ProjectsGallery.jsx
@@ -1,28 +1,33 @@
 "use client";
 
+import { useParams } from "react-router-dom";
 import { InfiniteMovingCards } from "./UI/MovingCards";
-
 
 const projects = [
   {
+    slug: "ikechukwu",
     src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763627962/port_ckjluh.png",
-    alt: "Project 1",
-    href: "https://ikechwukwu-portfolio.vercel.app/",
+    alt: "DEVCHUKS",
+    link: "/projects2/ikechukwu",
   },
   {
+    slug: "blinkist",
     src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763627842/Blink_zspbte.png",
-    alt: "Project 2",
+    alt: "BLINKIST",
     href: "https://blinkist-landing-page-seven.vercel.app/",
+    link: "/projects2/blinkist",
   },
   {
+    slug: "jshare",
     src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763633342/Screenshot_2025-11-20_105948_vnxvlg.png",
-    alt: "Project 3",
-    href: "https://j-upload.vercel.app",
+    alt: "JSHARE",
+    link: "/projects2/jshare",
   },
   {
+    slug: "medicx",
     src: "https://res.cloudinary.com/dydmptpcg/image/upload/v1763632234/Screenshot_2025-11-20_104944_uxotyh.png",
-    alt: "Project 4",
-    href: "https://medicx.app",
+    alt: "MEDICX",
+    link: "/projects2/medicx",
   },
 ];
 
