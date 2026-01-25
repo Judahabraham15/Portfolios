8e74d61de1301f036726cc3d210952887f1eb448
Judah Abraham <judahabrahamforgood@gmail.com>
2026-01-25 19:06:58 +0100

    Updated

--- Client/src/HomePage.jsx
+++ Client/src/HomePage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import About from "./Components/About";
 import Hero from "./Components/Hero";
 import Projects from "./Components/Projects";
-import Skills from "./Components/Skills";
 import Timeline from "./Components/Timeline/Timeline";
 import Contacts from "./Components/Contacts";
 import ScrollToTop from "./Components/ScrollToTop";
@@ -33,7 +32,7 @@ const HomePage = () => {
           <TechCarousel/>
           <About />
           <Projects />
-          {/* <Skills /> */}
+         
           <Timeline />
           <Contacts />
           <ScrollToTop />
