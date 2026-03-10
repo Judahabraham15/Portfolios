b3d2cacbe8f597f562f29607d2b70b63d0c28baf
Judah Abraham <judahabrahamforgood@gmail.com>
2026-03-10 15:19:49 +0100

    Remove unnecessary background color class from NavBody component

--- Client/src/Components/UI/ResizableNavbar.jsx
+++ Client/src/Components/UI/ResizableNavbar.jsx
@@ -61,7 +61,7 @@ export const NavBody = ({ children, className, visible }) => {
         minWidth: "800px",
       }}
       className={cn(
-        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-2.5 lg:flex dark:bg-white/4",
+        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-2.5 lg:flex",
         visible && "bg-neutral-950/80",
         className,
       )}
